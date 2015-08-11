19d9997c113b87f5f23ed8b58f2b3c8660b83528
Bryant <bryly27@sbcglobal.net>
2015-08-10 18:37:21 -0700

    added mailer

--- config/routes.js
+++ config/routes.js
@@ -2,14 +2,21 @@ var subs = require('../controllers/subs');
 
 module.exports = function(app) {
 	
-	app.get("/", function(req, res) {
+	app.get('/', function(req, res) {
 		// mongs.index(req, res);
-		res.render("index");
+		res.render('index');
 	});
 
-	app.post("/add", function(req, res){
-		console.log(req.body);
+	app.post('/add', function(req, res){
 		subs.add(req, res);
+	});
+
+	app.get('/unsubscribe', function(req, res){
+		res.render('unsubscribe');
+	});
+
+	app.post('/unsub', function(req, res){
+		subs.unsub(req, res);
 	});
 	
 }
--- controllers/subs.js
+++ controllers/subs.js
@@ -1,9 +1,14 @@
-// require mongoose and load the model that we are going to use
-// var path = require('path');
 var mongoose = require('mongoose');
 var Sub = mongoose.model('Subscriber');
+var nodemailer = require('nodemailer');
 
-
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
 
 
 // create an object with methods that we are going to export for our routes file to use
@@ -11,15 +16,51 @@ var subs = {}
 
 subs.add = function(req, res){
 
-	console.log(req.body);
-	var sub = new Sub(req.body);
-	sub.save(function(err, results){
+	Sub.findOne({email: req.body.email}, function(err, results){
 		if(err){
-			console.log("error", err);
+			console.log('error', err);
 		}else{
-			res.json(results);
+			console.log('results', results);
+			if(results === null){
+				var mailOptions = {
+			    from: 'Awkyo <awkyo.noreply@gmail.com>', // sender address 
+			    to: req.body.email, // list of receivers 
+			    subject: 'Thanks for Subscribing to Awkyo', // Subject line 
+			    html: '<p><strong>Welcome to Awkyo!</strong></p><p>Thank you for subscribing. We will keep you informed with new updates.</p><hr><p>If you did not subscribe, or would like to unsubscribe, please visit <a href="http://www.awkyo.com/unsubscribe">http://localhost:8001/unsubscribe</a></p>' // html body 
+				};
+
+				transporter.sendMail(mailOptions, function(error, info){
+			    if(error){
+			        return console.log(error);
+			    }
+			    console.log('Message sent: ' + info.response);
+				});
+
+				var sub = new Sub(req.body);
+				sub.save(function(err, results){
+					if(err){
+						console.log("error", err);
+					}else{
+						res.json({success: true});
+					}
+				})
+				
+			}else{
+				res.json({success: false});
+			}
 		}
-	})
+	});
+
+}, 
+
+subs.unsub = function(req, res){
+	Sub.remove({email: req.body.email}, function(err, results){
+		if(err){
+			console.log('error', err);
+		}else{
+			res.json({success: true});
+		}
+	});
 }
 
 
